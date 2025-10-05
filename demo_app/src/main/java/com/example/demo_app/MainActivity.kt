240a8079bac14961d95fd3fe0a78e439918cf5e0
pinball83 <raggamuffin.inda.shell@gmail.com>
2025-10-05 21:46:23 +0100

    Refactor code, simplify main activity, add MaskedEditText component

--- demo_app/src/main/java/com/example/demo_app/MainActivity.kt
+++ demo_app/src/main/java/com/example/demo_app/MainActivity.kt
@@ -24,15 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.toolbar)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null)
-                .setAnchorView(R.id.fab).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
